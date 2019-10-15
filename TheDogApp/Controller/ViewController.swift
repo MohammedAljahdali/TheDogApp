55f3c60ce2c4d48ee3276251ba7c364489f57282
MohammedAljahdali <mohammedkhalid4028@gmail.com>
2019-10-15 21:29:06 +0300

    Better refactoring of code

--- TheDogApp/Controller/ViewController.swift
+++ TheDogApp/Controller/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  TheDogApp
 //
-//  Created by Mohammed Khakidaljahdali on 14/10/2019.
+//  Created by Mohammed Khalid Aljahdali on 14/10/2019.
 //  Copyright © 2019 Mohammed. All rights reserved.
 //
 
@@ -13,48 +13,57 @@ class ViewController: UIViewController {
     @IBOutlet weak var dogImageView: UIImageView!
     
     @IBAction func changePicture(_ sender: Any) {
-        loadPicture()
+        requestImageUrl(url: randomDogUrl)
     }
     
     let randomDogUrl = DogAPI.UrlGenrator.randomDogPicture.url
     
-    func loadPicture() {
-        decodeUrl(url: randomDogUrl)
-    }
-    
-    func dogImage(dogData: DogData) {
-        let urlDogImage = URL(string: dogData.message)
-        let task = URLSession.shared.dataTask(with: urlDogImage!) { (data, response, error) in
-            guard let data = data else { print("error in dogImage func data"); return }
-            let dogImage = UIImage(data: data)
-            DispatchQueue.main.async {
-                self.dogImageView.image = dogImage
-            }
-            
-        }
-        task.resume()
-    }
-    
-    func decodeUrl(url: URL)  {
-        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
-            guard let data = data else { print("problem with data of url"); return }
-            let decode = JSONDecoder()
-            do {
-            let dogData = try decode.decode(DogData.self, from: data)
-                self.dogImage(dogData: dogData)
-            } catch {
-                print(error)
-            }
-        }
-        task.resume()
-        
-    }
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        loadPicture()
+        requestImageUrl(url: randomDogUrl)
+    }
+    
+    func requestImageUrl(url: URL) {
+        DogAPI.requestDogImageUrl(url: url, completionHandler: self.imageUrlHelper(url:error:) )
+    }
+    
+    func imageUrlHelper(url: URL?, error: Error?) {
+        guard let url = url else { print("nil url in ViewController/imageUrlHelper"); return }
+        DogAPI.requestDogImage(url: url, completionHandler: self.imageLoadHelper(image:error:))
+    }
+    
+    func imageLoadHelper(image: UIImage?, error: Error?) {
+        DispatchQueue.main.async {
+            self.dogImageView.image = image
+        }
     }
 
 
 }
-
+//    func dogImage(dogData: DogData) {
+//        let urlDogImage = URL(string: dogData.message)
+//        let task = URLSession.shared.dataTask(with: urlDogImage!) { (data, response, error) in
+//            guard let data = data else { print("error in dogImage func data"); return }
+//            let dogImage = UIImage(data: data)
+//            DispatchQueue.main.async {
+//                self.dogImageView.image = dogImage
+//            }
+//
+//        }
+//        task.resume()
+//    }
+//
+//    func decodeUrl(url: URL)  {
+//        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+//            guard let data = data else { print("problem with data of url"); return }
+//            let decode = JSONDecoder()
+//            do {
+//            let dogData = try decode.decode(DogData.self, from: data)
+//                self.dogImage(dogData: dogData)
+//            } catch {
+//                print(error)
+//            }
+//        }
+//        task.resume()
+//
+//    }
--- TheDogApp/Model/DogAPI.swift
+++ TheDogApp/Model/DogAPI.swift
@@ -7,6 +7,7 @@
 //
 
 import Foundation
+import UIKit
 
 class DogAPI {
     
@@ -16,5 +17,46 @@ class DogAPI {
         var url: URL {
             return URL(string: self.rawValue)!
         }
+    }
+    
+//    class func requestRandomDogImage() {
+//        requestDogImageUrl(url: UrlGenrator.randomDogPicture.url) { (url, error) in
+//            guard let url = url else { print("nil url in DogAPI/requestRandomDogImage"); return }
+//            requestDogImage(url: url) { (image, error) in
+//
+//            }
+//        }
+//    }
+    
+    class func requestDogImage(url: URL, completionHandler: @escaping (UIImage?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                completionHandler(nil, error)
+                print("error DogAPI/requestDogImage")
+                return
+            }
+            let dogImage = UIImage(data: data)
+            completionHandler(dogImage, nil)
+        }
+        task.resume()
+    }
+    
+    class func requestDogImageUrl(url: URL, completionHandler: @escaping (URL?, Error?) -> Void) {
+        let task = URLSession.shared.dataTask(with: url) { (data, response, error) in
+            guard let data = data else {
+                completionHandler(nil, error)
+                print("error DogAPI/requestDogImageUrl")
+                return
+            }
+            let decode = JSONDecoder()
+            do {
+                let dogData = try decode.decode(DogData.self, from: data)
+                let dogImageUrl = URL(string: dogData.message)
+                completionHandler(dogImageUrl, nil)
+            } catch {
+                print(error)
+            }
+        }
+        task.resume()
     }
 }
